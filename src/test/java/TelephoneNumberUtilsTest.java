ef622974b84b78f09b3cd617da29d3609e716fc2
Zeb Ford-Reitz <zeb.fordreitz@gmail.com>
2020-07-26 17:58:31 +0200

    Adds example tests

--- src/test/java/TelephoneNumberUtilsTest.java
+++ src/test/java/TelephoneNumberUtilsTest.java
@@ -5,8 +5,43 @@ import org.junit.jupiter.api.Test;
 public class TelephoneNumberUtilsTest {
 
 	@Test
-	void thisIsASampleFailingTest() {
-		fail("the name says it all, really");
+	public void testInvalidNumber() {
+		assertEquals("", TelephoneNumberUtils.extractExtension("+490531428239"));
+	}
+
+	@Test
+	public void testEmptyExtension() {
+		assertEquals("", TelephoneNumberUtils.extractExtension("+4912345678"));
+	}
+
+	@Test
+	public void testTwoDigitExtension() {
+		assertEquals("27", TelephoneNumberUtils.extractExtension("+491234567827"));
+	}
+
+	@Test
+	public void testThreeDigitExtension() {
+		assertEquals("372", TelephoneNumberUtils.extractExtension("+4912345678372"));
+	}
+
+	@Test
+	public void testFourDigitExtension() {
+		assertEquals("9999", TelephoneNumberUtils.extractExtension("+49123456789999"));
+	}
+
+	@Test
+	public void testOneDigitExtensionIsInvalid() {
+		assertEquals("", TelephoneNumberUtils.extractExtension("+49123456789"));
+	}
+
+	@Test
+	public void testFiveDigitExtensionIsInvalid() {
+		assertEquals("", TelephoneNumberUtils.extractExtension("+491234567899999"));
+	}
+
+	@Test
+	public void testTenDigitExtensionIsInvalid() {
+		assertEquals("", TelephoneNumberUtils.extractExtension("+49123456789999999999"));
 	}
 
 }
